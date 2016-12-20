744fadf8cbadfc11035b4fdead4fe1643575328f
Michal Kuffa <michal.kuffa@gmail.com>
2016-12-20 01:39:22 +0100

    Add initial version of OltClient
    * Initiate connection and interactive shell
    
      By default after the shell / channel is initiated login message is
      read so the channel is empty before any subsequent commands.
    
    * Execute commands and read the output
    
      Reading the input from interactive shell is a bit tricky since one never
      know if it's the end of a data. Therefore we wait for timeout and
      check if the channel is not closed. This should be sufficiet for
      commands which do not wait extremely long for the output on th OLT
      side.

---
+++ cisco_olt_client/client.py
@@ -0,0 +1,106 @@
+import socket
+try:
+    from io import BytesIO as StringIO
+except ImportError:
+    from StringIo import StringIO
+
+from paramiko.client import SSHClient, AutoAddPolicy
+
+from cisco_olt_client.exceptions import ConnectionError
+
+
+class OltClient:
+
+    def __init__(
+        self,
+        hostname,
+        username,
+        password,
+        auto_connect=True,
+        recv_timeout=1,
+        recv_chunk_size=4096,
+        missing_host_key_policy_cls=AutoAddPolicy
+    ):
+        self.hostname = hostname
+        self.username = username
+        self.password = password
+        self.auto_connect = auto_connect
+        self.recv_timeout = recv_timeout
+        self.recv_chunk_size = recv_chunk_size
+        self.missing_host_key_policy_cls = missing_host_key_policy_cls
+        self.shell = None
+        self.ssh_client = None
+
+    def get_ssh_client(self):
+        '''
+        Prepare default ssh client and set missing host key policy
+        '''
+        ssh_client = SSHClient()
+        ssh_client.set_missing_host_key_policy(
+            self.missing_host_key_policy_cls())
+        return ssh_client
+
+    def get_olt_shell(self, ssh_client, recv_msg=True):
+        '''
+        Initiate OLT interactive cli interface
+
+        :param bool recv_msg: Indicates that the login message (everything that
+            is printed by OLT on login) should be read so it will not appear in
+            output of subsequent command.
+
+        :return: OLT interactive cli interface in form of paramiko's channel
+        :rtype: :class:`paramiko.Channel`
+        :raises paramiko.AuthenticationException:
+        '''
+        ssh_client.connect(
+            self.hostname,
+            username=self.username,
+            password=self.password,
+            look_for_keys=False,
+            allow_agent=False,
+            timeout=5
+        )
+        shell = ssh_client.invoke_shell()
+        shell.settimeout(self.recv_timeout)
+        exec_command(shell, '')
+        return shell
+
+    def connect(self):
+        self.ssh_client = self.get_ssh_client()
+        self.shell = self.get_olt_shell(self.ssh_client)
+
+    def disconnect(self):
+        self.shell.close()
+        self.ssh_client.close()
+
+    def raw_exec_command(self, cmd, **exec_options):
+        if self.shell is None:
+            if self.auto_connect:
+                self.connect()
+            else:
+                raise ConnectionError(
+                    'Connectio not ready and auto_connect flag is False')
+        return exec_command(self.shell, cmd, **exec_options)
+
+
+def exec_command(
+    shell,
+    cmd,
+    buffer=None,
+    recv_chunk_size=4096,
+    output_as_file=False,
+    new_line=True,
+    new_line_char='\n'
+):
+    buffer = StringIO() if buffer is None else buffer
+    if not cmd.endswith(new_line_char):
+        cmd += new_line_char
+    shell.send(cmd)
+    while True:
+        try:
+            buffer.write(shell.recv(recv_chunk_size))
+        except socket.timeout:
+            if shell.closed:
+                raise ConnectionError('Connection to OTL failed')
+            break
+    return buffer.getvalue()
