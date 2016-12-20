e8ae0e51fad225fd540932f8a28efad9f29845c2
Michal Kuffa <michal.kuffa@gmail.com>
2016-12-20 01:36:34 +0100

    Change the main module docsctring

--- cisco_olt_client/__init__.py
+++ cisco_olt_client/__init__.py
@@ -1,4 +1,10 @@
-'''cisco_olt_client - Python wrapper for cisco's olt boxes commands executed via ssh'''
+'''
+================
+cisco_olt_client
+================
+
+Python wrapper for cisco's olt boxes commands executed via ssh.
+'''
 
 __version__ = '0.1.0'
 __author__ = 'Michal Kuffa <michal.kuffa@gmail.com>'
