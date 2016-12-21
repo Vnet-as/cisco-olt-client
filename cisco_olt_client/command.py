d64492c85182a992597729999f1e4483977b87d6
Michal Kuffa <michal.kuffa@gmail.com>
2016-12-21 23:58:53 +0100

    Add function for parsing cli arguments

--- cisco_olt_client/command.py
+++ cisco_olt_client/command.py
@@ -1,4 +1,24 @@
+import shlex
 import collections
+
+
+def arg2tuple(arg):
+    '''
+    Parse command line argument into name, value tuple
+    '''
+    name, value = arg.split('=', 1)
+
+    if name.startswith('--'):
+        name = name[2:]
+
+    # try to normalize string that's quoted
+    _value = shlex.split(value)
+    # other length than 1 means that the value string was either not properly
+    # quoted or is empty, in both cases we're not touching it
+    if len(_value) == 1:
+        value = _value[0]
+
+    return name, value
 
 
 class Command:
@@ -7,9 +27,12 @@ class Command:
         '''
         Single command and it's output representation
 
-        >>> c = Command('/remote-eq/status/show', ['--onuID=23', '--port=pon.7'])
+        >>> c = Command('/remote-eq/status', ['--onuID=23', '--port=pon.7'])
         >>> c.compile()
-        '/remote-eq/status/show --onuID=23 --port=pon.7'
+        '/remote-eq/status --onuID=23 --port=pon.7'
+
+        :param cmd: Full path to the command.
+        :type cmd: str
 
         :param args: Additional command arguments. In python2, if you need to
             preserve order of arguments use ordered dict or list of 2-tuples
