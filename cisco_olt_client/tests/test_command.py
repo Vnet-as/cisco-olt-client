f16441036307562e7a1cc1f364ea530701255bb8
Michal Kuffa <michal.kuffa@gmail.com>
2016-12-22 01:22:19 +0100

    Tests for arg parsing and compiling back

--- cisco_olt_client/tests/test_command.py
+++ cisco_olt_client/tests/test_command.py
@@ -1,11 +1,35 @@
-from cisco_olt_client.command import arg2tuple
+import shlex
+from cisco_olt_client.command import arg2tuple, args2mapping
 from cisco_olt_client.command import Command
 
 
+def test_args2mapping():
+    assert args2mapping(['--onuID=23'])['onuID'] == '23'
+    args = shlex.split('--onuID=23 --arg="string with spaces" --noval')
+    args_mapping = args2mapping(args)
+    assert args_mapping['onuID'] == '23'
+    assert args_mapping['arg'] == 'string with spaces'
+    assert args_mapping['noval'] is None
+    assert list(args_mapping.keys())[1] == 'arg'
+
+
 def test_arg2tuple():
+    assert arg2tuple('--onuID') == ('onuID', None)
     assert arg2tuple('--onuID=23') == ('onuID', '23')
+    assert arg2tuple('--onuID="this is a string with spaces"') == ('onuID', "this is a string with spaces") # noqa
 
 
+def test_args_none():
+    cmd = Command('/path/cmd')
+    assert not cmd.args
+
+def test_compile_arg_no_value():
+    cmd = Command('/path/cmd', dict(arg=None))
+    assert cmd.compile() == '/path/cmd --arg'
+
+    cmd = Command('/path/cmd --arg')
+    assert cmd.args['arg'] is None
+    assert cmd.compile() == '/path/cmd --arg'
 
 
 def test_simple_compile():
