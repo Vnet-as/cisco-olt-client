ddfafc904b5707e17bc30a55a11012e395adcd70
Michal Kuffa <michal.kuffa@gmail.com>
2016-12-22 23:57:15 +0100

    Add command related exceptions

--- cisco_olt_client/exceptions.py
+++ cisco_olt_client/exceptions.py
@@ -5,3 +5,16 @@ class CiscoOltClientException(Exception):
 
 class ConnectionError(CiscoOltClientException):
     '''Raised for unhandled / perceived connection errors'''
+
+
+class CommandError(CiscoOltClientException):
+    '''Command related errors base exception'''
+
+
+class CommandNotExecuted(CommandError):
+    '''
+    Command was not executed yet
+
+    Raised when attributes are requested that are usable only after the
+    command was executed. e.g. error, warning state
+    '''
